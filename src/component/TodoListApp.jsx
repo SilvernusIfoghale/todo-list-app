a75661057f4dafdd08ffcfccd89d87a57f2f61db
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2024-08-06 03:07:01 +0100

    setting up jsx code files

--- src/component/TodoListApp.jsx
+++ src/component/TodoListApp.jsx
@@ -1,5 +1,67 @@
 import React from "react";
 
 export default function TodoListApp() {
-  return <div>TodoListApp</div>;
+  return (
+    <>
+      <div className="flex justify-center items-center  w-full h-[100vh]">
+        <div className="w-[370px] sm:w-[450px] rounded-lg sm:h-[550px] h-[480px] px-5 bg-red-100 overflow-y-hidden">
+          <p className="font-bold text-2xl border-b-[1px] border-gray-300 pt-4 pb-1">
+            Todo
+          </p>
+          <div className="flex items-center gap-2">
+            <input
+              type="text"
+              value={task}
+              placeholder="Add a new task"
+              onChange={(e) => {
+                setTask(e.target.value);
+              }}
+              className="my-3 text-xs h-7 w-full px-2 bg-gray-100"
+            />
+            <button
+              className=" bg-blue-500 h-7 w-8 font-semibold text-gray-200"
+              onClick={handleSubmit}
+            >
+              +
+            </button>
+          </div>
+          {todo.map((myTodo, index) => {
+            return (
+              <div className="flex gap-1 items-center my-4" key={index}>
+                <span className="w-6 h-6">
+                  <img
+                    onClick={() => {
+                      handleComplete(myTodo.id);
+                    }}
+                    src={myTodo.isComplete ? checked : unChecked}
+                    alt=""
+                    className="w-full h-full object-contain"
+                  />
+                </span>
+                <p
+                  className="text-[1rem] font-semibold capitalize flex-1"
+                  style={{
+                    textDecoration: myTodo.isComplete && "line-through",
+                    color: myTodo.isComplete && "#9ca3af",
+                  }}
+                >
+                  {myTodo.task}
+                </p>
+                <span className="w-4 h-4">
+                  <img
+                    onClick={() => {
+                      handleDelete(myTodo.id);
+                    }}
+                    src={trash}
+                    alt=""
+                    className="w-full h-full object-contain"
+                  />
+                </span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
