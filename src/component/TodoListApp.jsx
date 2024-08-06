c982d9c31c9a0d3cbbe7d3aca8b1613c3fc6c59a
SilvernusIfoghale <silvernusorevaoghene@gmail.com>
2024-08-06 10:24:17 +0100

    setting up local storage

--- src/component/TodoListApp.jsx
+++ src/component/TodoListApp.jsx
@@ -1,12 +1,29 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import checked from "../../public/check.png";
 import unChecked from "../../public/uncheck.png";
 import trash from "../../public/trash.png";
 
 export default function TodoListApp() {
-  const [todo, setTodo] = useState([]);
+  //Getting Data from LocalStorage
+  const localData = () => {
+    let todoData = localStorage.getItem("todoData");
+    console.log("Todo data getting", todoData);
+    if (todoData) {
+      return (todoData = JSON.parse(localStorage.getItem("todoData")));
+    } else {
+      return [];
+    }
+  };
+
+  const [todo, setTodo] = useState(localData());
   const [task, setTask] = useState("");
 
+  //Saving Data to LocalStorage
+  useEffect(() => {
+    localStorage.setItem("todoData", JSON.stringify(todo));
+  }, [todo]);
+
+  //Submit a Task
   const handleSubmit = () => {
     if (task == "") {
       alert("Please enter a task");
@@ -20,6 +37,8 @@ export default function TodoListApp() {
       setTask("");
     }
   };
+
+  //Mark as Completed
   const handleComplete = (id) => {
     setTodo((currentTodo) =>
       currentTodo.map((task) => {
@@ -31,6 +50,8 @@ export default function TodoListApp() {
       })
     );
   };
+
+  //Delete Task
   const handleDelete = (id) => {
     const newTodo = todo.filter((task) => task.id != id);
     setTodo(newTodo);
@@ -59,7 +80,7 @@ export default function TodoListApp() {
               +
             </button>
           </div>
-          {todo.map((myTodo, index) => {
+          {todo?.map((myTodo, index) => {
             return (
               <div className="flex gap-1 items-center my-5" key={index}>
                 <span className="w-6 h-6">
